acfdcece3b4d79e8d20bd9cfdbc2a5b2f66f2f1f
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2024-07-07 22:50:12 -0300

    Conexão com o banco MySQL
    Foram feitas alguns ajustes no código e instalado o mysql via npm

--- pac_backend/app.js
+++ pac_backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import associadoRoutes from './routes/associadoRoutes.js';
+import mysql from 'mysql';
 
 const app = express();
 const port = 3000;
@@ -11,3 +12,17 @@ app.use('/api/associados', associadoRoutes);
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'UniversoDown',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Conectado ao banco de dados MySQL');
+});
+
+export default app;
