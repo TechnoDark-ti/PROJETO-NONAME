c972c93b138269b1d48e93bd791b2d378c369fc5
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2024-07-08 15:48:02 -0300

    update
    Deu tudo errado kkkk reiniciando o front

--- pac_frontend/universo-down-frontend/src/App.vue
+++ pac_frontend/universo-down-frontend/src/App.vue
@@ -1,29 +1,26 @@
 <template>
-  <div id="app">
-    <NavbarComponent />
-    <router-view/>
-    <FooterComponent />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import NavbarComponent from './components/Navbar.vue';
-import FooterComponent from './components/Footer.vue';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    NavbarComponent,
-    FooterComponent
+    HelloWorld
   }
-};
+}
 </script>
 
 <style>
-@import '~bootstrap/dist/css/bootstrap.css';
-@import '~bootstrap-vue-3/dist/bootstrap-vue-3.css';
-
-html, body, #app {
-  height: 100%;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
